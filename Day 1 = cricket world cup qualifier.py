37958a70d711d467a922982413e00c0d98883ff7
Bejsakak <krishnamiku57@gmail.com>
2024-09-28 18:23:42 +0530

    Add files via upload

---
+++ Day 1 = cricket world cup qualifier.py
@@ -0,0 +1,8 @@
+x=int(input("enter the team's score:"))
+#check if the scores id 12 or more 
+if x=12:
+    print("yes")
+    #the team qualifies for the next round 
+    esle:
+    print("no")
+    #the team does not qualify 
